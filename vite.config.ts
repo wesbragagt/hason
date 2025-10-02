748c2dacf7f72f6165f4c9d461ad57684aaa47f5
wesbragagt <wesbragagt@example.com>
2025-10-01 22:15:42 -0500

    batman

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'hason',
+      short_name: 'hason',
+      description: 'Your friendly neighbor json formatter.',
+      theme_color: '#66C7F4',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
